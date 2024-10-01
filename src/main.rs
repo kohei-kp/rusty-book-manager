1642472e5c0005c9f647bebd4f604af1a7495033
kohei-kp <cowhey1991@gmail.com>
2024-10-01 23:03:49 +0900

    feat: health_checkの実装(3.3.5)

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 use tokio::net::TcpListener;
 
-// ハンドラ
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+// リクエストがくると 200 OK を返す
+async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     // ルーターを作成
-    let app = Router::new().route("/hello", get(hello_world));
+    let app = Router::new().route("/health", get(health_check));
 
     // localhost:8080 でリクエストを受け付ける
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     // 指定アドレスでリスナーを作成
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
 
     println!("Listening on {}", addr);
 
     // サーバーを起動
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
 }
