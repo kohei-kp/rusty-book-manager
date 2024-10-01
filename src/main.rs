a55e48fc322e2f06369b4c26058c7cfd7689cab7
kohei-kp <cowhey1991@gmail.com>
2024-10-01 22:41:49 +0900

    feat: axumでHelloworld。(3.2.5)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{Ipv4Addr, SocketAddr};
+
+use axum::{routing::get, Router};
+use tokio::net::TcpListener;
+
+// ハンドラ
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[tokio::main]
+async fn main() {
+    // ルーターを作成
+    let app = Router::new().route("/hello", get(hello_world));
+
+    // localhost:8080 でリクエストを受け付ける
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
+    // 指定アドレスでリスナーを作成
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    println!("Listening on {}", addr);
+
+    // サーバーを起動
+    axum::serve(listener, app).await.unwrap();
 }
