0b1924fe7cc953f25c00a7072ad9d9639c9f4e32
kohei-kp <cowhey1991@gmail.com>
2024-10-04 14:37:01 +0900

    feat: 簡易的なDIコンテナの実装(4.7.2)

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::sync::Arc;
+
+use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
+use kernel::repository::health::HealthCheckRepository;
+
+// DIコンテナの役割を果たす構造体定義
+#[derive(Clone)]
+pub struct AppRepository {
+    pub health_check_repository: Arc<dyn HealthCheckRepository>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl AppRepository {
+    pub fn new(pool: ConnectionPool) -> Self {
+        // 依存解決
+        let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        Self {
+            health_check_repository,
+        }
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    // 依存解決したインスタンスを返すメソッド
+    pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
+        self.health_check_repository.clone()
     }
 }
