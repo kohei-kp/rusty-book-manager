a890749dda97f45b6798fa47d0f2c24ef8d8522c
kohei-kp <cowhey1991@gmail.com>
2024-10-06 21:13:26 +0900

    feat: registoryの実装。AppRegistoryへの追加(5.2.6)

--- registry/src/lib.rs
+++ registry/src/lib.rs
@@ -1,25 +1,36 @@
 use std::sync::Arc;
 
-use adapter::{database::ConnectionPool, repository::health::HealthCheckRepositoryImpl};
-use kernel::repository::health::HealthCheckRepository;
+use adapter::{
+    database::ConnectionPool,
+    repository::{book::BookRepositoryImpl, health::HealthCheckRepositoryImpl},
+};
+use kernel::repository::{book::BookRepository, health::HealthCheckRepository};
 
 // DIコンテナの役割を果たす構造体定義
 #[derive(Clone)]
 pub struct AppRegistry {
     pub health_check_repository: Arc<dyn HealthCheckRepository>,
+    pub book_repository: Arc<dyn BookRepository>,
 }
 
 impl AppRegistry {
     pub fn new(pool: ConnectionPool) -> Self {
         // 依存解決
         let health_check_repository = Arc::new(HealthCheckRepositoryImpl::new(pool.clone()));
+        let book_repository = Arc::new(BookRepositoryImpl::new(pool.clone()));
+
         Self {
             health_check_repository,
+            book_repository,
         }
     }
 
     // 依存解決したインスタンスを返すメソッド
     pub fn health_check_repository(&self) -> Arc<dyn HealthCheckRepository> {
         self.health_check_repository.clone()
+    }
+
+    pub fn book_repository(&self) -> Arc<dyn BookRepository> {
+        self.book_repository.clone()
     }
 }
