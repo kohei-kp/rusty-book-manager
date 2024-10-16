123f20e2d40d54f07fd4b4f3bd8f0f91ebdb4a6e
kohei-kp <cowhey1991@gmail.com>
2024-10-16 22:48:32 +0900

    feat: ログ出力の実装(5.3.2)

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,16 +1,26 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
-use anyhow::{Error, Result};
+use anyhow::{Error, Result, Context};
+use tower_http::trace::{
+    DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer,
+};
+use tower_http::LatencyUnit;
+use tracing::Level;
 use api::route::{book::build_book_routers, health::build_health_check_routers};
-
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
 use tokio::net::TcpListener;
 
+use shared::env::{which, Environment};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
 }
 
@@ -23,12 +33,56 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routers())
         .merge(build_book_routers())
+        .layer(cors())
+        .layer(
+            TraceLayer::new_for_http()
+            .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+            .on_request(DefaultOnRequest::new().level(Level::INFO))
+            .on_response(
+                DefaultOnResponse::new()
+                    .level(Level::INFO)
+                    .latency_unit(LatencyUnit::Millis)
+            ),
+        )
         .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;
 
-    println!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
 
-    axum::serve(listener, app).await.map_err(Error::from)
+    axum::serve(listener, app).await
+        .context("Unexpected error happended in server")
+        .inspect_err(|e| {
+            tracing::error!((
+                    error.cause_chain = ?e,
+                    error.message = %e,
+                    "Unexpected error"
+            )
+        })
+}
+
+// ロガーを初期化する関数
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+
+    // ログレベルのを判定
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    // ログの出力形式を設定
+    let subscriber = tracing_subscriber::fmt()::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
