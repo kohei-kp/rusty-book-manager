17cdc0d835d6471c23c838b725d4856a921da76d
kohei-kp <cowhey1991@gmail.com>
2024-11-03 16:46:11 +0900

    feat: 蔵書データへの貸出情報追加(5.7)

--- kernel/src/model/book/mod.rs
+++ kernel/src/model/book/mod.rs
@@ -1,4 +1,8 @@
-use crate::model::{id::BookId, user::BookOwner};
+use crate::model::{
+    id::{BookId, CheckoutId},
+    user::{BookOwner, CheckoutUser},
+};
+use chrono::{DateTime, Utc};
 
 pub mod event;
 
@@ -10,10 +14,21 @@ pub struct Book {
     pub isbn: String,
     pub description: String,
     pub owner: BookOwner,
+    pub checkout: Option<Checkout>,
 }
 
 #[derive(Debug)]
 pub struct BookListOptions {
     pub limit: i64,
     pub offset: i64,
+}
+
+// model::checkout側でも同名の型を定義しているが、
+// それとは異なるモジュールにあるので別の型として扱われるa。
+// 実際、上記`Book`方のcheckoutフィールドとしてのみ使用する。
+#[derive(Debug)]
+pub struct Checkout {
+    pub checkout_id: CheckoutId,
+    pub checked_out_by: CheckoutUser,
+    pub checked_out_at: DateTime<Utc>,
 }
